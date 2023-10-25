35ef5f9cbf3dca083d368b605ccc3626c2f58757
Luciano <lumacias11702@gmail.com>
2023-10-24 21:40:13 -0300

    sucursal y uml

---
+++ SistemaGlobal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCorporacionParaguas
+{
+    class SistemaGlobal
+    {
+        private List<Sucursal> sucursales;
+        public SistemaGlobal()
+        {
+            sucursales = new List<Sucursal>();
+        }
+        public void CrearSucursal(string nombre)
+        {
+            sucursales.Add(new Sucursal(nombre));
+        }
+        public List<Sucursal> ListarSucursales() 
+        {
+            List<Sucursal> sRet = new List<Sucursal>();
+            sRet.AddRange(sucursales);
+            return sRet; // la clono aunq las sucursales son iguales para no modificar referencias fuera
+        }
+        public List<Virus> ListarVirus()
+        {
+            List<Virus> virusRet = new List<Virus>();
+            foreach (Sucursal s in sucursales)
+            {
+                virusRet.AddRange(s.Virus);
+            }
+            return virusRet;
+        }
+        public List<Vacuna> ListarVacunas()
+        {
+            List<Vacuna> vacunasRet = new List<Vacuna>();
+            foreach(Sucursal s in sucursales)
+            {
+                vacunasRet.AddRange(s.Vacunas);
+            }
+            return vacunasRet;
+        }
+        public void AutodestruirSucursal(string nombre)
+        {
+            bool encontre = false;
+            int indice = 0;
+            while (!encontre)
+            {
+                if (sucursales[indice].Nombre == nombre) encontre = true;
+                else indice++;
+            }
+            sucursales[indice].AutodestruirSucursal();
+        }
+        public void AutodestruirGlobal()
+        {
+            foreach (Sucursal s in sucursales)
+            {
+                s.AutodestruirSucursal();
+            }
+        }
+    }
+}
--- Sucursal.cs
+++ Sucursal.cs
@@ -10,5 +10,52 @@ namespace EjercicioCorporacionParaguas
     {
         public List<Vacuna> Vacunas { get; private set; }
         public List<Virus> Virus { get; private set; }
+        public string Nombre { get; private set; }
+        public Sucursal(string nombre)
+        {
+            Nombre = nombre;
+            Vacunas = new List<Vacuna>();
+            Virus = new List<Virus>();
+        }
+        public void SintetizarVirus()
+        {
+            int posUltimaVacuna = Vacunas.Count - 1;
+            Virus nuevoVirus = Vacunas[posUltimaVacuna].SintetizarVirus();
+            Virus.Add(nuevoVirus);
+        }
+        public void AgregarVacuna(string nombre, Designaciones designacion)
+        {
+            Vacunas.Add(new Vacuna(nombre, designacion));
+        }
+        public void DestruirMedicamento(string Id)
+        {
+            bool encontre = false;
+            int indice = 0;
+            while (!encontre)
+            {
+                if (Vacunas[indice].Id == Id) encontre = true;
+                else indice++;
+            }
+            Vacunas[indice].Destruir();
+        }
+        public void DestruirTipo(Designaciones designacion)
+        {
+            List<Medicamento> listado = new List<Medicamento>();
+            listado.AddRange(Vacunas);
+            listado.AddRange(Virus);
+
+            foreach (Medicamento v in listado)
+            {
+                if (v.Designacion == designacion) v.Destruir();
+            }
+        }
+        public void AutodestruirSucursal()
+        {
+            Array array = Enum.GetValues(typeof(Designaciones));
+            foreach (Designaciones d in array)
+            {
+                DestruirTipo(d);
+            }
+        }
     }
 }
